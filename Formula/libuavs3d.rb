4881ee9b9e55a10a75033539c0b4d6b1c0d993c9
herbertgao <heapcn@gmail.com>
2023-02-23 16:19:55 +0800

    feat: support libuavs3d

---
+++ Formula/libuavs3d.rb
@@ -0,0 +1,18 @@
+class Libuavs3d < Formula
+  desc ""
+  homepage ""
+  url "https://github.com/HerbertGao/uavs3d/archive/refs/tags/1.2_1.tar.gz"
+  sha256 "b050e0b093e472a495067820ccad5dbc4e4ed5ec4e80639c69def4954e7ca825"
+  license ""
+
+  depends_on "gawk" => :build
+  depends_on "cmake" => :build
+
+  def install
+    system "mkdir", "build/linux"
+    system "cd", "build/linux"
+    system "cmake", "-DCOMPILE_10BIT=1", "../.."
+    system "make"
+    system "make", "install"
+  end
+end

997c0d486b5e7af707b15d0c13c9e0f20e02fde8
herbertgao <heapcn@gmail.com>
2023-02-23 16:56:30 +0800

    feat: support libuavs3d

--- Formula/libuavs3d.rb
+++ Formula/libuavs3d.rb
@@ -11,11 +11,12 @@ class Libuavs3d < Formula
   def install
     # ENV.deparallelize  # if your formula fails when building in parallel
 
-    system "mkdir", "build/linux"
-    cd "build/linux" do
-      system "cmake", "-DCOMPILE_10BIT=1", "../.."
-      system "make"
-      system "make", "install"
+    mkdir "build/linux" do
+      cd "build/linux" do
+        system "cmake", "-DCOMPILE_10BIT=1", "../.."
+        system "make"
+        system "make", "install" # if this fails, try separate make/make install steps
+      end
     end
   end
 end

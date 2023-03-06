14529c3aea9c7b6f133fe8450029c4023eec203d
herbertgao <heapcn@gmail.com>
2023-03-06 11:26:45 +0800

    fix: uavs3d.rb

--- Formula/uavs3d.rb
+++ Formula/uavs3d.rb
@@ -1,21 +1,19 @@
 class Uavs3d < Formula
-  desc ""
-  homepage ""
+  desc "uavs3d library"
+  homepage "https://github.com/uavs3/uavs3d"
   url "https://github.com/HerbertGao/uavs3d/archive/refs/tags/1.2_1.tar.gz"
   sha256 "b050e0b093e472a495067820ccad5dbc4e4ed5ec4e80639c69def4954e7ca825"
   license ""
 
-  depends_on "gawk" => :build
   depends_on "cmake" => :build
+  depends_on "gawk" => :build
 
   def install
-    # ENV.deparallelize  # if your formula fails when building in parallel
-
     system "mkdir", "build/linux"
     cd "build/linux" do
       system "cmake", "-DCMAKE_INSTALL_PREFIX=#{prefix}", "-DCOMPILE_10BIT=1", "../.."
       system "make"
-      system "make install"
+      system "make", "install"
     end
   end
 end
